ce9cb1590cf77324726f7e420ca1070c5bef2e3a
IgorDePaula <principe.borodin@gmail.com>
2013-07-30 17:03:45 -0300

    Configuracao para documentos de email

--- src/MailService/Service/MailService.php
+++ src/MailService/Service/MailService.php
@@ -2,21 +2,83 @@
 
 namespace MailService\Service;
 
-use Zend\Mail\Message;
+use Zend\Mime\Part as MimeType;
+use Zend\Mail\Message as Mail;
+use Zend\Mime\Message as Mime;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
-class MailService extends Message {
+class MailService {
 
-    private $mailservice;
+    private $mailservice = null;
+    private $template = null;
+    private $mail = null;
+    private $serviceLocator = null;
 
-    public function __construct(ServiceLocatorInterface $sm) {
+    public function __construct(ServiceLocatorInterface $sm, $template) {
+        $this->mail = new Mail();
+        $this->setTemplate($template);
+        $this->setServiceLocator($sm);
+
+
         $this->mailservice = $sm->get('MailService');
-        $this->setEncoding("UTF-8");    
     }
 
-    public function send()
-    {
-        $this->mailservice->send($this);
+    public function getServiceLocator() {
+        return $this->serviceLocator;
+    }
+
+    public function setServiceLocator(ServiceLocatorInterface $serviceLocator) {
+        $this->serviceLocator = $serviceLocator;
+    }
+
+    public function send() {
+
+        $headers = $this->mail->getHeaders();
+        $headers->removeHeader('Content-Type');
+        $headers->addHeaderLine('Content-Type', 'text/html; charset=UTF-8');
+        $this->mail->setHeaders($headers);
+        $this->mailservice->send($this->mail);
+    }
+
+    public function getTemplate() {
+        return $this->template;
+    }
+
+    public function setTemplate($template) {
+        $this->template = $template;
+        return $this;
+    }
+
+    public function addFrom($from) {
+        $this->mail->addFrom($from);
+        return $this;
+    }
+
+    public function addTo($to) {
+        $this->mail->addTo($to);
+        return $this;
+    }
+
+    public function setSubject($subject) {
+        $this->mail->setSubject($subject);
+    }
+
+    public function setBody(array $body) {
+        $renderer = $this->getServiceLocator()->get('ViewRenderer');
+        $content = $renderer->render($this->getTemplate(), $body);
+        $mimehtml = new MimeType($content);
+        $message = new Mime();
+        $message->addPart($mimehtml);
+        $this->mail->setBody($message);
+        return $this;
+    }
+
+    public function addBcc($bcc) {
+        $this->mail->addBcc($bcc);
+    }
+
+    public function addCc($cc) {
+        $this->mail->addCc($cc);
     }
 
 }
---
+++ src/MailService/Service/ServiceTemplate.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace MailService\Service;
+
+class ServiceTemplate {
+    
+   const ENVIO_TESTE =  'envio/teste';
+   
+   const HELPDESK_ABERTURA =  'helpdesk/abertura';
+   
+   const HELPDESK_FECHAMENTO =  'helpdesk/fechamento';
+   
+   const HELPDESK_RESPOSTA =  'helpdesk/resposta';
+   
+}
