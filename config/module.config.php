e12a09dd850013c7045f1e58f50f84f74511c72f
IgorDePaula <principe.borodin@gmail.com>
2013-08-02 08:18:02 -0300

    Insercao dos templates

--- config/module.config.php
+++ config/module.config.php
@@ -14,7 +14,26 @@ return array(
         ),
     ),
   
-    
+    'view_manager' => array(
+        'display_not_found_reason' => true,
+        'display_exceptions' => true,
+        'doctype' => 'HTML5',
+        'not_found_template' => 'error/404',
+        'exception_template' => 'error/index',
+        'template_map' => array(
+            'layout/layout' => realpath(__DIR__ . '/../../base/view/layout/layout.phtml'),
+            'application/index/index' => __DIR__ . '/../../base/view/application/index/index.phtml',
+          
+            'envio/teste' => __DIR__ . '/../view/envio/teste.phtml',
+            'helpdesk/resposta' => __DIR__ . '/../view/helpdesk/email-resposta-chamado.phtml',
+            'helpdesk/abertura' => __DIR__ . '/../view/helpdesk/email-abertura-chamado.phtml',
+            'helpdesk/fechamento' => __DIR__ . '/../view/helpdesk/email-fechar-chamado.phtml',
+            
+        ),
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
     
    
 );
